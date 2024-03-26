56b1560d8aec752cad4a2acf41df53e0224dcdc4
Luis Fagundes <lhfagundes@gmail.com>
2024-03-26 19:16:09 -0300

    Send incremental updates to frontend

--- rxdjango/signal_handler.py
+++ rxdjango/signal_handler.py
@@ -95,6 +95,7 @@ class SignalHandler:
                 serialized['_operation'] = 'create' if created else 'update'
                 self._schedule(serialized, _layer)
 
+
         def relay_instance(sender, instance, **kwargs):
             if sender is layer.model:
                 tstamp = sync_get_tstamp()
@@ -182,8 +183,8 @@ class SignalHandler:
             anchors = state_model.get_anchors(serialized)
         for anchor in anchors:
             #print(f'... to {anchor.__class__.__name__} {anchor}')
-            self.wsrouter.sync_dispatch(payload, anchor.id, user_id)
-            self.mongo.write_instances(anchor.id, payload)
+            deltas = self.mongo.write_instances(anchor.id, payload)
+            self.wsrouter.sync_dispatch(deltas, anchor.id, user_id)
 
     def broadcast_instance(self, anchor_id, instance, operation='update'):
         kwargs = {}
