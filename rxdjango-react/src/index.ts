c8fb5e707972bc0dfa2e830f36351f23341b1d08
Luis Fagundes <lhfagundes@gmail.com>
2024-04-10 10:13:17 -0300

    fix imports

--- rxdjango-react/src/index.ts
+++ rxdjango-react/src/index.ts
@@ -1,4 +1,5 @@
 export { default as StateChannel } from './StateChannel';
-export { default as InstanceHandler } from './InstanceHandler';
 
 export type { InstanceType } from './StateChannel.d';
+
+export { useStateChannel } from './hook';
